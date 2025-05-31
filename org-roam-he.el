5ddfd86e9f5bc61007a4ba9396f21c0f9358678a
Tomke Pfoch <pfoch.tomke@gmail.com>
2025-06-01 01:06:13 +0200

    query hyper edges

--- org-roam-he.el
+++ org-roam-he.el
@@ -50,8 +50,6 @@
 		(:foreign-key [node-via-id] :references nodes [id] :on-delete :cascade))))
 
 
-
-
 (defun org-roam-he-insert-link (link)
   "Insert rel-link data for LINK at current point into the Org-roam cache."
   (save-excursion
@@ -75,42 +73,117 @@
 	  t)))))
 
 (defun org-roam-he-sort-promote (nodes)
-  "Return sort function which displays NODES at top when using org-roam-node-read."
+  "Return sort function which displays NODES at top for `org-roam-node-read'."
   (let ((ids (--map (org-roam-node-id it) nodes)))
     (lambda (completion-a _)
       (-contains-p ids (org-roam-node-id (cdr completion-a))))))
 
 (defvar org-roam-he-nodes
-  '((node-a . "Node A")
-    (node-via . "Relation")
-    (node-b . "Node B")))
+  '((node-a-id . "Node A")
+    (node-via-id . "Relation")
+    (node-b-id . "Node B")))
 
-(defun org-roam-he--prompt-id (he-node &optional must-match)
-  "Prompt user for HE-NODE Node while displaying node at point at the top.
+
+(defun org-roam-he--prompt-node (he-node &optional must-match)
+  "Prompt user for HE-NODE Node while showing node at point at the top.
 If MUST-MATCH is non-nil the selected Node must exist.
-Return Node-id to of the selected node"
-  (org-roam-node-id
-   (org-roam-node-read nil nil
-		       (org-roam-he-sort-promote (list (org-roam-node-at-point)))
-		       must-match
-		       (concat (cdr (assoc he-node org-roam-he-nodes)) ": "))))
+Return org-roam-node to of the selected node.
+If no node was selected return nil."
+  (let ((node (org-roam-node-read nil nil
+		      (org-roam-he-sort-promote (list (org-roam-node-at-point)))
+		      must-match
+		      (concat (cdr (assoc he-node org-roam-he-nodes)) ": "))))
+    (when (org-roam-node-id node) node)))
 
-(defun org-roam-he-query ()
-  "Find all Hyper-Edges"
-  (let ((selection 
-	 (-fix (lambda (l)
-		 (let ((pending (-filter (lambda (e)
-					   (not (--any-p (eq (car it) e) l)))
-					 '(node-a node-via node-b))))
-		   (if (< (length l) 2)
-		       (cons (--any (when-let (id (org-roam-he--prompt-id it))
-				      (cons it id))
-				    pending)
-			     l)
-		     l)))
-	       nil)))
-    selection))
 
+(defun org-roam-he--max-title-length (he-node selected collection)
+  "Get maximal length of HE-NODE title for alignment.
+Called by `org-roam-he--prompt-query' wth SELECTED and COLLECTION."
+  (if-let (assoc-node (assoc he-node selected))
+      (length (org-roam-node-title (cdr assoc-node)))
+  (-max (--map (length (org-roam-node-title (cdr (assoc he-node it)))) collection))))
+
+
+;; TODO allow custom render function to get alignment (max length)
+;; TODO optimize render function
+(defun org-roam-he--prompt-query (selected collection &optional renderfn)
+  "Prompt user to select Hyper-Edge from COLLECTION narrowed to SELECTED.
+
+Optionally a custom display function can be used with RENDERFN."
+  (-let* ((render
+	   (or renderfn
+	       (let* ((node-a-max-length
+		       (org-roam-he--max-title-length 'node-a-id selected collection))
+		      (node-via-max-length
+		       (org-roam-he--max-title-length 'node-via-id selected collection)))
+		 (-lambda ((&alist 'node-a-id a 'node-via-id v 'node-b-id b))
+		   (let ((node-a-text
+			  (string-pad (org-roam-node-title a) node-a-max-length))
+			 (node-via-text
+			  (string-pad (org-roam-node-title v) node-via-max-length))
+			 (node-b-text (org-roam-node-title b)))
+		     (concat (if (assoc 'node-a-id selected)
+				 (propertize node-a-text 'face 'shadow)
+			       node-a-text)
+			     " ⟵ "
+			     (if (assoc 'node-via-id selected)
+				 (propertize node-via-text 'face 'shadow)
+			       node-via-text)
+			     " ⟶ "
+			     (if (assoc 'node-b-id selected)
+				 (propertize node-b-text 'face 'shadow)
+			       node-b-text) ))))))
+	  
+	  (nodes (--map (cons (funcall render (-concat selected it)) it) collection))
+	  (node (completing-read "Node: " nodes nil t)))
+    (cdr (assoc node nodes))))
+
+
+(defun org-roam-he--query (selected variable)
+  "Query org-roam DB for Hyper-Edges using SELECTED fields to narrow edges.
+
+When selecting from table only use fields in VARIABLE.
+The user is also prompted for queried edges using a `completing-read'."
+  (let* ((selected-ids (--map (list (car it) (org-roam-node-id (cdr it)))
+			      selected))
+	 (ids
+	  (cond
+	   ((= 0 (length selected))
+	    (apply 'org-roam-db-query [:select [$i1 $i2 $i3] :from hyper-edges] variable))
+	   ((= 1 (length selected))
+	    (apply 'org-roam-db-query [:select [$i1 $i2] :from hyper-edges
+					       :where (= $i3 $s4)]
+		   (-concat variable (car selected-ids))))
+	   ((= 2 (length selected))
+	    (apply 'org-roam-db-query [:select [$i1] :from hyper-edges
+					       :where (AND (= $i2 $s3) (= $i4 $s5))]
+		   (-concat variable (-flatten selected-ids))))))
+	 (collection (-map (lambda (e)
+			     (-zip-pair variable (--map (org-roam-node-from-id it) e)))
+			   ids)))
+    (org-roam-he--prompt-query selected collection)))
+
+
+(defun org-roam-he-find-oriented ()
+  "Query Hyper-Edges in roam Database.
+
+User will be prompted for NODE-A, RELATION and NODE-B any one of which
+can be left empty to widen selection.  A Database Query will be issued
+using the selected nodes for narrowing.  User will be prompted with
+ramaining nodes."
+  (let ((selection nil)
+	(variable nil))
+    (--each-while org-roam-he-nodes
+	(> 2 (length selection))
+      (if-let (node (org-roam-he--prompt-node (car it)))
+	  (push (cons (car it) node) selection)
+	(push (car it) variable)))
+    (let* ((nodes (--map (cdr it) (org-roam-he--query selection variable)))
+	   (-compare-fn (-on 'equal 'org-roam-node-id))
+	   (filter (lambda (node) (-contains-p nodes node)))
+	   (choice (org-roam-node-read nil filter nil t "visit: ")))
+      (org-roam-node-visit choice))))
+	  
 
 (advice-add 'org-roam-db-insert-link :before-until 'org-roam-db-insert-he-link)
 
@@ -118,12 +191,12 @@ Return Node-id to of the selected node"
 (defun org-roam-he-follow (rel-link)
   "Follow a roamh: Hyper-Edge Link REL-LINK.
 
-The User will be promted for which node to visit,
-the node at point will be ignored.
+The User will be promted for which node to visit, the node at point will
+be ignored.
 
 rel-link should have the format [[roamh:node-a-id:node-via-id:node-b-id]].
 
-Consider using org-roam-he-insert to create a link using the wizard."
+Consider using `org-roam-he-insert' to create a link using the wizard."
   
   (when-let* ((current (org-roam-id-at-point))
 	      (targets (--filter (not (equal current it)) (string-split rel-link ":")))
@@ -136,12 +209,12 @@ Consider using org-roam-he-insert to create a link using the wizard."
 
 The user will be promted to select NODE-A, RELATION and NODE_B
 as well as a description.  The node at point is displayed at top
-using org-roam-he-sort-promote."
+using `org-roam-he-sort-promote'."
   (interactive)
   (-when-let* ((all-nodes (org-roam-node-list))
 	       ((node-a node-via node-b)
-		(--map (org-roam-he--prompt-id it t)
-		 '("Node A" "Relation" "Node B")))
+		(--map (org-roam-node-id (org-roam-he--prompt-node (car it) t))
+		 org-roam-he-nodes))
 	       (description (read-string "Description: ")))
     (org-insert-link nil (concat "roamh:" node-a ":" node-via ":" node-b) description)))
 (org-link-set-parameters "roamh" :follow #'org-roam-he-follow)
